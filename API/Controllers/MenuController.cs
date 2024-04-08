8ba472c5ced9386c212559cb4b557e9ab39ffae2
NgocMach36 <machngoc3600@gmail.com>
2024-04-08 14:06:18 +0700

    Api menu

---
+++ API/Controllers/MenuController.cs
@@ -0,0 +1,86 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using API.Repositories;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class MenuController : BaseApiController
+    {
+        private readonly IMenuRepository _menuRepository;
+        private readonly IMapper _mapper;
+
+        public MenuController(IMenuRepository menuRepository, IMapper mapper)
+        {
+            _menuRepository = menuRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MenuDto>> Create(MenuDto menuDto)
+        {
+            var menu = _mapper.Map<Menu>(menuDto);
+
+            await _menuRepository.CreateMenuItemAsync(menu);
+            await _menuRepository.SaveAllAsync();
+
+            return menuDto;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MenuDto>>> GetMenu()
+        {
+            var menuItems = await _menuRepository.GetMenuAsync();
+            return Ok(menuItems);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MenuDto>> GetMenuItem(int id)
+        {
+            var menuItem = await _menuRepository.GetMenuItemByIdAsync(id);
+            if (menuItem == null)
+            {
+                return NotFound("Menu item not found");
+            }
+            return Ok(_mapper.Map<MenuDto>(menuItem));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, MenuDto menuDto)
+        {
+            var menuItem = await _menuRepository.GetMenuItemByIdAsync(id);
+            if (menuItem == null)
+            {
+                return NotFound("Menu item not found");
+            }
+            _mapper.Map(menuDto, menuItem);
+
+            if (await _menuRepository.SaveAllAsync()) 
+                return NoContent();
+
+            return BadRequest("Failed to update menu item");
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var menuItem = await _menuRepository.GetMenuItemByIdAsync(id);
+            if (menuItem == null)
+            {
+                return NotFound("Menu item not found");
+            }
+
+            _menuRepository.DeleteMenuItem(menuItem);
+            if (await _menuRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to delete menu item");
+        }
+    }
+}
---
+++ API/Data/MenuRepository.cs
@@ -0,0 +1,44 @@
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class MenuRepository : IMenuRepository
+    {
+        private readonly DataContext _context;
+
+        public MenuRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Menu> CreateMenuItemAsync(Menu menu)
+        {
+            await _context.Menus.AddAsync(menu);
+            return menu;
+        }
+
+        public void DeleteMenuItem(Menu menu)
+        {
+            _context.Menus.Remove(menu);
+        }
+
+        public async Task<IEnumerable<Menu>> GetMenuAsync()
+        {
+            return await _context.Menus.ToListAsync();
+        }
+
+        public async Task<Menu> GetMenuItemByIdAsync(int id)
+        {
+            return await _context.Menus.FindAsync(id);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
